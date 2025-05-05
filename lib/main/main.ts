f51d7457b8553b208b6b28094cfef5a7488622a3
Necati Meral <necati_meral@yahoo.de>
2025-05-05 22:16:37 +0200

    initial auto-update

--- lib/main/main.ts
+++ lib/main/main.ts
@@ -2,11 +2,25 @@ import { electronApp, optimizer } from '@electron-toolkit/utils'
 import { app, BrowserWindow } from 'electron'
 import { installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer'
 import log from 'electron-log/main'
+import electronUpdater, { type AppUpdater } from 'electron-updater'
 import { registerNativeThemeEventListeners } from '../window/ipcEvents'
 import getOrCreateAppWindow, { createAppWindow } from './app'
 
 // initialize the logger for any renderer process
 log.initialize()
+
+export function getAutoUpdater(): AppUpdater {
+  // Using destructuring to access autoUpdater due to the CommonJS module of 'electron-updater'.
+  // It is a workaround for ESM compatibility issues, see https://github.com/electron-userland/electron-builder/issues/7976.
+  const { autoUpdater } = electronUpdater
+
+  autoUpdater.logger = log
+  autoUpdater.disableWebInstaller = true
+
+  return autoUpdater
+}
+
+getAutoUpdater().checkForUpdatesAndNotify()
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
